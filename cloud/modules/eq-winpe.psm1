9b8a5362f1fdde4ab1765fcbafe9e43a34423320
Petinh0 <petinho92@gmail.com>
2025-07-24 13:36:10 +0200

    a

--- cloud/modules/eq-winpe.psm1
+++ cloud/modules/eq-winpe.psm1
@@ -16,7 +16,7 @@ function osdcloud-WinpeInstallCurl {
     [CmdletBinding()]
     param ()
     if (-not (Get-Command 'curl.exe' -ErrorAction SilentlyContinue)) {
-        Write-Host -ForegroundColor Yellow "[-] Install Curl 8.1.2 for Windows (CDM)"
+        Write-Host -ForegroundColor Yellow "[-] Install Curl 8.1.2 for Windows (CDM2)"
         #$Uri = 'https://curl.se/windows/dl-7.81.0/curl-7.81.0-win64-mingw.zip'
         #$Uri = 'https://curl.se/windows/dl-7.88.1_2/curl-7.88.1_2-win64-mingw.zip'
         #$Uri = 'https://curl.se/windows/dl-8.1.2_2/curl-8.1.2_2-win64-mingw.zip'
@@ -30,7 +30,7 @@ function osdcloud-WinpeInstallCurl {
     }
     else {
         $GetItemCurl = Get-Item -Path "$env:SystemRoot\System32\curl.exe" -ErrorAction SilentlyContinue
-        Write-Host -ForegroundColor Green "[+] Curl $($GetItemCurl.VersionInfo.FileVersion)"
+        Write-Host -ForegroundColor Green "[+] Curl $($GetItemCurl.VersionInfo.FileVersion) (CDM2)"
     }
 }
 function osdcloud-WinpeInstallPowerShellGet {
