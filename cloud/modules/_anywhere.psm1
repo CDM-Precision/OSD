a34ea7714e42edd71ddcfa8edfd7ed4f76c8e986
Petinh0 <petinho92@gmail.com>
2025-07-28 10:16:18 +0200

    update

--- cloud/modules/_anywhere.psm1
+++ cloud/modules/_anywhere.psm1
@@ -318,17 +318,37 @@ function osdcloud-InstallPowerShellModule {
             Write-Host -ForegroundColor Green "[+] $Name $($InstalledModule.Version) (ert)"
         }
     }
-    else{
-        $CDMModuleName = 'OSD'
-        Write-Host -ForegroundColor Yellow "[-] Install OSD(CDM) for Windows"
-        $Uri = 'https://github.com/CDM-Precision/OSD/archive/refs/heads/master.zip'
-        Invoke-WebRequest -UseBasicParsing -Uri $Uri -OutFile "$env:TEMP\osd.zip"
-        $null = New-Item -Path "$env:TEMP\$CDMModuleName" -ItemType Directory -Force
-        Expand-Archive -Path "$env:TEMP\osd.zip" -DestinationPath "$env:TEMP\$CDMModuleName"
-        $null = New-Item -Path "$env:ProgramFiles\WindowsPowerShell\Modules\$CDMModuleName" -ItemType Directory -ErrorAction SilentlyContinue
-        Move-Item -Path "$env:TEMP\$CDMModuleName" -Destination "$env:ProgramFiles\WindowsPowerShell\Modules\$CDMModuleName"
-        Import-Module $CDMModuleName -Force -Scope Global
+    else {
+    $CDMModuleName = 'OSD'
+    Write-Host -ForegroundColor Yellow "[-] Install OSD(CDM) for Windows"
+    $zipPath = "$env:TEMP\osd.zip"
+    $extractPath = "$env:TEMP\$CDMModuleName"
+    $modulePath = "$env:ProgramFiles\WindowsPowerShell\Modules\$CDMModuleName"
+
+    # download zip
+    Invoke-WebRequest -UseBasicParsing -Uri 'https://github.com/CDM-Precision/OSD/archive/refs/heads/master.zip' -OutFile $zipPath
+
+    # expand zip
+    Expand-Archive -Path $zipPath -DestinationPath $extractPath -Force
+
+    
+    $sourcePath = Get-ChildItem -Path $extractPath | Where-Object { $_.PSIsContainer } | Select-Object -First 1
+
+    # remove dest if exists
+    if (Test-Path $modulePath) {
+        Remove-Item -Path $modulePath -Recurse -Force
     }
+
+    # create dest dir
+    New-Item -Path $modulePath -ItemType Directory -Force | Out-Null
+
+    # move
+    Move-Item -Path "$($sourcePath.FullName)\*" -Destination $modulePath
+
+    # import module
+    Import-Module $CDMModuleName -Force -Scope Global
+}
+
 }
 
 function osdcloud-RestartComputer {
