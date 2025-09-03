2d088ad11410a930cf874598cf236a6cfbf3636a
Petinh0 <petinho92@gmail.com>
2025-09-03 11:36:05 +0200

    comment console write

--- Public/OSDCloud.ps1
+++ Public/OSDCloud.ps1
@@ -986,7 +986,7 @@
                     
                     Write-Host -ForegroundColor Yellow "Load Download module"
                     Invoke-Expression (Invoke-RestMethod -Uri 'https://raw.githubusercontent.com/CDM-Precision/CDM-DownloadFile/refs/heads/main/DownloadFile.psm1')
-                    Write-Host -ForegroundColor Yellow "OK"
+                    1Write-Host -ForegroundColor Yellow "OK"
 
                     if ($OSDCloudUSB -and $Global:OSDCloud.OSVersion -and $Global:OSDCloud.OSReleaseID) {
                         $OSDownloadChildPath = "$($OSDCloudUSB.DriveLetter):\OSDCloud\OS\$($Global:OSDCloud.OSVersion) $($Global:OSDCloud.OSReleaseID)"
@@ -1008,19 +1008,19 @@
                         {
                             New-Item -ItemType Directory -Path 'C:\OSDCloud\OS' -Force
                         }
-                        Write-DarkGrayHost -Message "Valami else ag"
+                        #Write-DarkGrayHost -Message "Valami else ag"
                         Save-WebFile2 -URL $Global:OSDCloud.ImageFileUrl -OutFile $downloadedFilePath -RetryCount 10
                         $Global:OSDCloud.ImageFileDestination = Get-Item -Path $downloadedFilePath
-                        Write-DarkGrayHost -Message "ASD ImageDest: [($Global:OSDCloud.ImageFileDestination)]"
+                        #Write-DarkGrayHost -Message "ASD ImageDest: [($Global:OSDCloud.ImageFileDestination)]"
                     }
                 }
                 else {
                     #$Global:OSDCloud.ImageFileDestination = Save-WebFile -SourceUrl $Global:OSDCloud.ImageFileUrl -DestinationDirectory 'C:\OSDCloud\OS' -ErrorAction Stop
                     $downloadedFilePath = Join-Path -Path 'C:\OSDCloud\OS' -ChildPath $Global:OSDCloud.ImageFileName
-                        Write-DarkGrayHost -Message "Masike else ag"
+                        #Write-DarkGrayHost -Message "Masike else ag"
                         Save-WebFile2 -URL $Global:OSDCloud.ImageFileUrl -OutFile $downloadedFilePath -RetryCount 10
                         $Global:OSDCloud.ImageFileDestination = Get-Item -Path $downloadedFilePath
-                        Write-DarkGrayHost -Message "AS2D ImageDest: [($Global:OSDCloud.ImageFileDestination)]"
+                        #Write-DarkGrayHost -Message "AS2D ImageDest: [($Global:OSDCloud.ImageFileDestination)]"
                 }
                 if (!(Test-Path $Global:OSDCloud.ImageFileDestination.FullName)) {
                     $Global:OSDCloud.ImageFileDestination = Get-ChildItem -Path 'C:\OSDCloud\OS\*' -Include *.wim,*.esd,*.iso | Select-Object -First 1
